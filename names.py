f3d9ce429159c30451e57f506f7edad936309ba1
kumar0912 <kumar.94@iitj.ac.in>
2020-06-22 20:10:59 +0530

    Deleted some files

--- names.py
+++
@@ -1,7 +0,0 @@
-from name_function import formatted_name
-
-first = input("Please enter the first name")
-last = input("Please enter the last name")
-
-result = formatted_name(first,last)
-print(result)
