3e3e6c991fef4ab7f527ea53dc987fb4be91e8ec
kumar0912 <kumar.94@iitj.ac.in>
2020-06-22 20:26:18 +0530

    Corrected

--- test_validate_request.py
+++ test_validate_request.py
@@ -3,19 +3,19 @@ from validate_request import validate_request
 
 class RequestTestCase(unittest.TestCase):
 #left is just to make the request objects and rest is done
-    def test_missing_info():
+    def test_missing_info(self):
         result = validate_request(request)
         self.assertEqual(result,0)
-    def incorrect_info():
+    def incorrect_info(self):
         result = validate_request(request)
         self.assertEqual(result,0)
 
 class SettleTestCase(unittest.TestCase):
 #left is just to make the request objects and rest is done
-    def test_missing_info():
+    def test_missing_info(self):
         result = validate_request(request)
         self.assertEqual(result,0)
-    def incorrect_info():
+    def incorrect_info(self):
         result = validate_request(request)
         self.assertEqual(result,0)
 
